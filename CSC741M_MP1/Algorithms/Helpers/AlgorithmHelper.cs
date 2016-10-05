f6128596cbe1d98690a10a79df069cbe5c477b50
Evan Livelo <evan.livelo@gmail.com>
2016-10-06 02:58:20 +0800

    Tracked AlgorithmHelper

---
+++ CSC741M_MP1/Algorithms/Helpers/AlgorithmHelper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC741M_MP1.Algorithms.Helpers
+{
+    public class AlgorithmHelper
+    {
+        public static LUVClass[,] convertImageToLUV(string path)
+        {
+            Bitmap image = new Bitmap(path);
+
+            LUVClass[,] convertedImage = new LUVClass[image.Height, image.Width];
+
+            for (int i = 0; i < image.Height; i++)
+            {
+                for (int j = 0; j < image.Width; j++)
+                {
+                    Color c = image.GetPixel(j, i);
+                    CIEConvert cie = new CIEConvert();
+                    cie.setValues(c.R, c.G, c.B);
+                    convertedImage[i, j] = cie.finalLUV;
+                }
+            }
+
+            return convertedImage;
+        }
+
+        public static Dictionary<int, double> generateLUVHistogram(LUVClass[,] image)
+        {
+            Dictionary<int, double> histogram = new Dictionary<int, double>();
+            CIEConvert cie = new CIEConvert();
+
+            for (int i = 0; i < image.GetLength(0); i++)
+            {
+                for (int j = 0; j < image.GetLength(1); j++)
+                {
+                    int key = cie.LuvIndexOf(image[i, j]);
+                    if (histogram.ContainsKey(key))
+                    {
+                        histogram[key] += 1.0;
+                    }
+                    else
+                    {
+                        histogram.Add(key, 1.0);
+                    }
+                }
+            }
+
+            double totalPixels = histogram.Sum(x => x.Value);
+
+            foreach (int key in histogram.Keys.ToList())
+            {
+                histogram[key] /= totalPixels;
+            }
+
+            return histogram;
+        }
+
+        public static bool isSimilar(Dictionary<int, double> query, Dictionary<int, double> data, double threshold)
+        {
+            Dictionary<int, double> compilation = new Dictionary<int, double>();
+            for (int i = 0; i < query.Count; i++)
+            {
+                int queryKey = query.Keys.ElementAt(i);
+                compilation.Add(queryKey, 0.0);
+                double dataValue = 0.0;
+                if (data.ContainsKey(queryKey))
+                {
+                    dataValue = data[queryKey];
+                    data.Remove(queryKey);
+                }
+                compilation[queryKey] = 1 - Math.Abs((query[queryKey] - dataValue) / Math.Max(query[queryKey], dataValue));
+            }
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                int dataKey = data.Keys.ElementAt(i);
+                compilation.Add(dataKey, 0.0);
+            }
+
+            double total = 0.0;
+            int keyCount = compilation.Keys.Count;
+            foreach (int key in compilation.Keys)
+            {
+                total += compilation[key];
+            }
+            total /= keyCount;
+
+            return total >= threshold;
+        }
+    }
+}
